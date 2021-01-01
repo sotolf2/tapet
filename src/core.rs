b82a60bdae4975fea39a9d535b9846b8de67f01d
Sotolf2 <trym.karlsen@protonmail.ch>
2021-01-01 14:42:45 +0100

    Generalized out random file from folder and added random from download

--- src/core.rs
+++ src/core.rs
@@ -6,10 +6,8 @@ use std::path::Path;
 use rand::seq::SliceRandom;
 use crate::config::Config;
 
-
-pub fn random_favorite(config: &Config) -> String {
-    let favorite_string = String::from(shellexpand::tilde(&config.tapet.favorites_folder));
-    let folder_path = Path::new(&favorite_string);
+fn random_from_folder(folder_str: &str) -> String{
+    let folder_path = Path::new(&folder_str);
     let mut paths: Vec<String> = Vec::new();
     for entry in fs::read_dir(&folder_path).expect("Couldn't find favourite folder") {
         let entry = entry.expect("couldn't find file");
@@ -21,6 +19,16 @@ pub fn random_favorite(config: &Config) -> String {
         None => String::from(""),
         Some(str) => String::from(str)
     }
+}
+
+pub fn random_favorite(config: &Config) -> String {
+    let favorite_string = String::from(shellexpand::tilde(&config.tapet.favorites_folder));
+    random_from_folder(&favorite_string)
+}
+
+pub fn random_downloaded(config: &Config) -> String {
+    let downloaded_string = String::from(shellexpand::tilde(&config.tapet.downloads_folder));
+    random_from_folder(&downloaded_string)
 }
 
 pub fn ensure_folders(config: &Config) -> Result<(), Error>{
